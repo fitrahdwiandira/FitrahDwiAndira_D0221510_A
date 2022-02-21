9fc7ffecbbd8ad273b64e3f90ec0d6b2797ac8e4
Fitrah Dwi Andira <99630480+fitrahdwiandira@users.noreply.github.com>
2022-02-21 15:50:36 +0800

    Add files via upload

---
+++ day3_java_if_else.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class day3_java_if_else {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2 != 0){
+            System.out.println("Weird");
+        }else if (N >= 2 && N<=5){
+            System.out.println("Not Weird");
+        }else if (N >= 6 && N<=20){
+            System.out.println("Weird");
+        }else if (N >20){
+            System.out.println("Not Weird");
+        }
+    }
+}
