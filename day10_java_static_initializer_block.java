5e8f74dce3d938958399ef50efb570e0c61bd095
Fitrah Dwi Andira <99630480+fitrahdwiandira@users.noreply.github.com>
2022-02-28 23:09:55 +0800

    Add files via upload

---
+++ day10_java_static_initializer_block.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class day10_java_static_initializer_block {
+
+static Scanner scan = new Scanner(System.in);
+static int B = scan.nextInt();
+static int H = scan.nextInt();
+static boolean flag = true;
+static{
+    try{
+        if(B <= 0 || H <= 0){
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }catch(Exception e){
+        System.out.println(e);
+    }
+
+}
+public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+        
+    }
+
+}
