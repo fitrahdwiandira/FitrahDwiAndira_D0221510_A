1cd670381c7836ec24cf9870b363cc61be9fe6ab
Fitrah Dwi Andira <99630480+fitrahdwiandira@users.noreply.github.com>
2022-03-11 19:24:14 +0800

    Add files via upload

---
+++ day21_Java_Regex.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class day21_Java_Regex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+    }
+}
+
+//Write your code here
+class MyRegex{
+    public String pattern = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\b)";
+}
