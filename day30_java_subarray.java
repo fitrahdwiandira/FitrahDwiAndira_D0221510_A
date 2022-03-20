a8272db90834817ba6e947ee81e274637329c918
Fitrah Dwi Andira <99630480+fitrahdwiandira@users.noreply.github.com>
2022-03-21 00:39:11 +0800

    Add files via upload

---
+++ day30_java_subarray.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[]A= new int[n];
+        for (int i=0;i<n;i++){
+            A[i]=scanner.nextInt();
+        }
+        
+        int nNegativeSum = 0;
+       for(int i=0;i<A.length;i++){//start
+            for(int j=i;j<A.length;j++){//end
+                int sum = 0;
+                for(int k=i;k<=j;k++){
+                    sum += A[k]; 
+                }
+                if(sum<0){
+                    nNegativeSum++; 
+                }
+                //System.out.println(" = "+sum);
+            }
+       }   
+    System.out.println(nNegativeSum);            
+    }
+} 
